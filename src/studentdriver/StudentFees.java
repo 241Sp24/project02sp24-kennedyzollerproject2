65b5f5eec42a0c42a24e595794db93c22dbcc005
PKennedy08 <pjkennedy08@gmail.com>
2024-04-05 10:33:15 -0500

    student fee class
    student fee class

--- src/studentdriver/StudentFees.java
+++ src/studentdriver/StudentFees.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package studentdriver;
 
-/**
- *
- * @author S559619
- */
+
 public class StudentFees {
+    private String studentName;
+    private int studentID;
+    private boolean isEnrolled;
+    private int CREDITS_PER_COURSE = 3;
+    private double PER_CREDIT_FEE = 543.50;
+    
+    public StudentFees(String studentName, int studentID, boolean isEnrolled){
+        
+        
+    }
+    public String getStudentName(){
+        return studentName;
+    }
+    public int getStudentID(){
+        return studentID;
+    }
+    public boolean isIsEnrolled(){
+        return isEnrolled;
+    }
+    public int getCREDITS_PER_COURSE(){
+        return CREDITS_PER_COURSE;
+    }
+    public double getPER_CREDIT_FEE(){
+        return PER_CREDIT_FEE;
+    }
+
+    /**
+     * @param studentName the studentName to set
+     */
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    /**
+     * @param studentID the studentID to set
+     */
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    /**
+     * @param isEnrolled the isEnrolled to set
+     */
+    public void setIsEnrolled(boolean isEnrolled) {
+        this.isEnrolled = isEnrolled;
+    }
+    
     
 }
