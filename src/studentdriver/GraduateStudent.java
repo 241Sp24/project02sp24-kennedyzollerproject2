94e7b0edee2642e22ee759ecb95d512748c21c02
LoganZoller <logan.zoller05@gmail.com>
2024-04-09 12:23:34 -0500

    classes
    c

--- src/studentdriver/GraduateStudent.java
+++ src/studentdriver/GraduateStudent.java
@@ -9,5 +9,40 @@ package studentdriver;
  * @author S559619
  */
 public class GraduateStudent {
+    private int coursesEnrolled;
+    private boolean isGraduateAssistant;
+    private String graduateAssistantType;
+    private double ADDITIONAL_FEES = 654.45;
     
+    public GraduateStudent(String studentName, int studentID, boolean isEnrolled, boolean isGraduateAssistant, 
+            String graduateAssistantType, int coursesEnrolled){
+        this.studentName = studentName;
+        this.studentID = studentID;
+        this.isEnrolled = isEnrolled;
+        this.isGraduateAssistant = isGraduateAssistant;
+        this.graduateAssistantType = graduateAssistantType;
+        this.coursesEnrolled = coursesEnrolled;
+    }
+    
+    public GraduateStudent(String studentName, int studentID, boolean isEnrolled, 
+            boolean isGraduateAssistant, int coursesEnrolled){
+        
+    }
+    
+    public boolean isIsGraduateAssistant(){
+        return isGraduateAssistant; 
+    }
+    
+    public int getCoursesEnrolled(){
+        return coursesEnrolled;
+    }
+    
+    public double getPayableAmount(){
+        return payableAmount;
+    }
+    
+    public String toString(){
+        return
+    }
+                   
 }
--- src/studentdriver/UGStudent.java
+++ src/studentdriver/UGStudent.java
@@ -8,6 +8,41 @@ package studentdriver;
  *
  * @author S559619
  */
+
 public class UGstudent {
+    private double scholarshipAmount;
+    private int coursesEnrolled;
+    private boolean hasScholarship;
+    private double ADDITIONAL_FEE = 820.70;
     
+    
+    public UGstudent(String studentName, int studentID, boolean isEnrolled, boolean hasScholarship, 
+        double scholarshipAmount, int coursesEnrolled){
+        this.studentName = studentName;
+        this.studentID = studentID;
+        this.isEnrolled = isEnrolled;
+        this.hasScholarship = hasScholarship;
+        this.scholarshipAmount = scholarshipAmount;
+        this.coursesEnrolled = coursesEnrolled;
+    }
+    
+    public boolean isHasScholarship(){
+        return hasScholarship;
+    }
+    
+    public double getScholarshipAmount(){
+        return scholarshipAmount;
+    }
+    
+    public int getCoursesEnrolled(){
+        return coursesEnrolled;
+    }
+    
+    public double getPayableAmount(){
+        return payableAmount;
+    }
+    
+    public String toString(){
+        return 
+    }
 }
